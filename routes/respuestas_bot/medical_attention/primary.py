3974fdc8bf34c114e7c5bdccb50d4f6997e18c21
AndresDBF <andres200605@gmail.com>
2024-06-21 14:12:56 -0400

    cambio de id en volver

--- routes/respuestas_bot/medical_attention/primary.py
+++ routes/respuestas_bot/medical_attention/primary.py
@@ -105,7 +105,7 @@ def confirm_call(numero):
                     {
                         "type": "reply",
                         "reply": {
-                            "id": "volver",
+                            "id": "idvolver",
                             "title": "Volver al inicio"
                         }
                     },
